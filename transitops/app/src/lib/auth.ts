f7dba41618c0fa0a96b3ce9a79f0c81b48da8c5a
CodexKnight-ai <148789243+CodexKnight-ai@users.noreply.github.com>
2026-07-12 11:09:33 +0530

    Add Prisma singleton instance

---
+++ transitops/app/src/lib/auth.ts
@@ -0,0 +1,20 @@
+// lib/auth.ts
+import { SignJWT, jwtVerify } from "jose";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+const secretKey = process.env.JWT_SECRET_KEY; 
+const key = new TextEncoder().encode(secretKey);
+
+export async function encrypt(payload: any) {
+    return await new SignJWT(payload)
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .setExpirationTime("2h")
+        .sign(key);
+}
+
+export async function decrypt(input: string): Promise<any> {
+    const { payload } = await jwtVerify(input, key, { algorithms: ["HS256"] });
+    return payload;
+}
